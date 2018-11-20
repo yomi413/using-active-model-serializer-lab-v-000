cbb6cf534e9ad216fe897705e5ad6388e38a3917
Yomaira Escano <yjescano@gmail.com>
2018-11-20 04:52:13 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,6 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    # render json: @product, status: 200
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @product}
